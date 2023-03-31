9171c6321248684affdc97431b307dd4f3f38ee0
Terence Brouns <t.s.n.brouns@gmail.com>
2023-03-31 21:43:32 +0200

    refactoring

--- python/infer_chatgpt.py
+++ python/infer_chatgpt.py
@@ -1,132 +1,148 @@
-# Databricks notebook source
-# !pip install openai
-
-# COMMAND ----------
-
 import configparser
 import csv
 import glob
-import numpy as np
 import os
-import pandas as pd
 import openai
 import time
 from tqdm import tqdm
 
 from utils import parse_txt, convert_to_dict, convert_dict_to_txt
 
-n_cards = 20
 
-data_dir = "./data/"
-os.makedirs(data_dir, exist_ok=True)
+if __name__ == "__main__":
 
-config_path = "config.ini"
-cfg = configparser.ConfigParser()
-cfg.read(config_path)
-openai.api_key = cfg["openai"]["api_key"]
+    # Read the config
+    config_path = "config.ini"
+    assert os.path.isfile(), f"No {config_path} present! Please create it."
+    cfg = configparser.ConfigParser()
+    cfg.read(config_path)
+    openai.api_key = cfg["openai"]["api_key"]
 
-category_dict = {}
-with open("categories.csv", "r") as f:
-    reader = csv.reader(f, delimiter=';')
-    for line in reader:
-        n_samples_for_category, category = line
-        category_dict[category] = int(n_samples_for_category)
-categories = list(category_dict.keys())
-n_categories = len(categories)
+    # Constants
+    n_cards = int(cfg["general"]["n_cards"])
+    data_dir = cfg["general"]["data_dir"]
 
-for category_index in tqdm(range(n_categories)):
+    # Create temp directories
+    os.makedirs(data_dir, exist_ok=True)
 
-    category = categories[category_index]
-    n_samples_for_category = category_dict[category]
-    txt_path = os.path.join(data_dir, f"{category}.txt")
+    # Read the categories from the csv
+    category_dict = {}
+    with open("categories.csv", "r") as f:
+        reader = csv.reader(f, delimiter=';')
+        for line in reader:
+            n_samples_for_category, category = line
+            category_dict[category] = int(n_samples_for_category)
+    categories = list(category_dict.keys())
+    n_categories = len(categories)
 
-    n_samples_old = -n_cards
+    # Generate words in each category using ChatGPT
 
-    print(f"\nProcessing '{category}'...")
+    for category_index in tqdm(range(n_categories)):
 
-    # Get existing words
-    if os.path.isfile(txt_path):
-        data_dict_existing = parse_txt(txt_path)
-    else:
-        data_dict_existing = None
+        # Get the category and the number of words to generate
+        category = categories[category_index]
+        n_samples_for_category = category_dict[category]
+        print(f"\nProcessing '{category}'...")
 
-    while True:
+        # Output text path
+        txt_path = os.path.join(data_dir, f"{category}.txt")
 
-        if data_dict_existing is not None:
-            taboo_words = list(data_dict_existing.keys())
-            n_samples = len(taboo_words)
-            taboo_words = ", ".join(taboo_words)
+        # Get existing words
+        if os.path.isfile(txt_path):
+            data_dict_existing = parse_txt(txt_path)
         else:
-            data_dict_existing = {}
-            n_samples = 0
-            taboo_words = None
+            data_dict_existing = None
 
-        if n_samples >= n_samples_for_category or n_samples <= n_samples_old + n_cards // 2:
-            print(
-                "\n"
-                f"Stopped processing...\n"
-                f"Samples in current step: {n_samples}\n"
-                f"Samples in previous step: {n_samples_old}\n"
-                f"Total samples: {n_samples_for_category}"
-            )
-            break
-        else:
-            print(f"Progress: {n_samples} / {n_samples_for_category}")
+        # Initialize samples old
+        n_samples_old = -n_cards
 
-        n_samples_old = n_samples
+        # Keep generating words in this category until ...
+        # ... we get the minimum number of words, or we cannot generate any more words
+        while True:
 
-        content = (
-            f"Genereer {n_cards} speelkaartjes voor het spel 'Taboo' in de categorie: '{category}'.\n\n"
-            "Gebruik de volgende voorbeeldlayout voor elk kaartje:\n\n"
-            "Te raden: Auto; "
-            "Verboden woorden: "
-            "Voertuig, "
-            "Wielen, "
-            "Rijden, "
-            "Wagen, "
-            "Straat"
-        )
+            if data_dict_existing is not None:
+                # Get existing words to not generate again
+                taboo_words = list(data_dict_existing.keys())
+                n_samples = len(taboo_words)
+                taboo_words = ", ".join(taboo_words)
+            else:
+                data_dict_existing = {}
+                n_samples = 0
+                taboo_words = None
 
-        if taboo_words is not None:
-            content += (
-                "\n\nGenereer geen van de volgende woorden om te raden: "
-                f"{taboo_words}"
-            )
-
-        response = None
-        while response is None:
-            try:
-                response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=[
-                        {"role": "user", "content": content},
-                    ],
+            # Check if we should stop processing
+            if n_samples >= n_samples_for_category or n_samples <= n_samples_old + n_cards // 2:
+                print(
+                    "\n"
+                    f"Stopped processing...\n"
+                    f"Samples in current step: {n_samples}\n"
+                    f"Samples in previous step: {n_samples_old}\n"
+                    f"Total samples: {n_samples_for_category}"
                 )
-            except openai.error.APIError as e:
-                print(e)
-                print("Sleeping...")
-                time.sleep(30)
+                break
+            else:
+                print(f"Progress: {n_samples} / {n_samples_for_category}")
 
-        if response is not None:
-            output = response.choices[0].message.content
-            lines = output.split("\n")
-            data_dict_existing = data_dict_existing | convert_to_dict(lines)
+            # Create the prompt
+            content = (
+                f"Genereer {n_cards} speelkaartjes voor het spel 'Taboo' in de categorie: '{category}'.\n\n"
+                "Gebruik de volgende voorbeeldlayout voor elk kaartje:\n\n"
+                "Te raden: Auto; "
+                "Verboden woorden: "
+                "Voertuig, "
+                "Wielen, "
+                "Rijden, "
+                "Wagen, "
+                "Straat"
+            )
 
-    # Save to file
-    lines = convert_dict_to_txt(data_dict_existing)
-    with open(txt_path, "w") as f:
+            # Add exclusion clause to prompt
+            if taboo_words is not None:
+                content += (
+                    "\n\nGenereer geen van de volgende woorden om te raden: "
+                    f"{taboo_words}"
+                )
+
+            # Do inference with ChatGPT
+            response = None
+            while response is None:
+                # Keep trying to get a response in case of error
+                try:
+                    response = openai.ChatCompletion.create(
+                        model="gpt-3.5-turbo",
+                        messages=[
+                            {"role": "user", "content": content},
+                        ],
+                    )
+                except openai.error.APIError as e:
+                    print(e)
+                    print("Sleeping...")
+                    time.sleep(30)
+
+            # If we get a reponse, add to the dictionary
+            if response is not None:
+                output = response.choices[0].message.content
+                lines = output.split("\n")
+                data_dict_existing = data_dict_existing | convert_to_dict(lines)
+
+            # Keep track of the number of samples in previous iteration
+            n_samples_old = n_samples
+
+        # Convert dictionary to txt and save to disk
+        lines = convert_dict_to_txt(data_dict_existing)
+        with open(txt_path, "w") as f:
+            for line in lines:
+                line = line.strip()
+                f.write(line)
+                f.write("\n")
+
+    # Combine all txt files into single txt
+    txt_file_list = glob.glob(os.path.join(data_dir, "*.txt"))
+    with open("words.txt", 'w') as f:
+        data_dict = {}
+        for txt_path in txt_file_list:
+            data_dict = data_dict | parse_txt(txt_path)
+        lines = convert_dict_to_txt(data_dict)
         for line in lines:
-            line = line.strip()
             f.write(line)
             f.write("\n")
-
-# Convert txt files
-txt_file_list = glob.glob(os.path.join(data_dir, "*.txt"))
-with open("words.txt", 'w') as f:
-    data_dict = {}
-    for txt_path in txt_file_list:
-        data_dict = data_dict | parse_txt(txt_path)
-    lines = convert_dict_to_txt(data_dict)
-    for line in lines:
-        f.write(line)
-        f.write("\n")
--- python/utils.py
+++ python/utils.py
@@ -1,30 +1,42 @@
 def convert_to_dict(lines, min_chars=10):
+    """
+    Convert the output from LLM or CSV content to dictionary
+    :param lines: output lines from LLM following structure from prompt or CSV content
+    :param min_chars: minimum numbers of characters in a line
+    """
     data_dict = {}
     prev_line = None
     for line in lines:
         line = line.strip()
+        # TODO: parametrize the prefix and split_pattern
         prefix = "Te raden"
         split_pattern = "; Verboden woorden: "
         if prefix in line and split_pattern not in line:
+            # In case the words for a single card are split between two lines ...
             prev_line = line
             process_line = False
         elif prefix not in line and split_pattern in line:
+            # ... we combine with the previous line because the words are split between two lines
             line = f"{prev_line} {line}"
             process_line = True
         elif len(line) > min_chars:
+            # Heuristic ...
             process_line = True
         else:
+            # The line doesn't have enough characters, so we ignore it
             process_line = False
         if process_line:
             prev_line = None
-            if split_pattern in line:
+            if split_pattern in line:  # LLM output
+                # Split the line at the provided prompt cue
                 guess_word, taboo_words = line.split(split_pattern)
                 # Get the guess word
                 guess_word = guess_word.split(": ")[-1]
                 # Get the taboo words
                 taboo_words = taboo_words.split(",")
                 taboo_words = [w.rstrip('\n').strip() for w in taboo_words]
-            else:
+            else:  # CSV structure
+                # Process the CSV line
                 words = line.split(",")
                 words = [w.strip() for w in words]
                 guess_word = words[0]
@@ -35,10 +47,18 @@ def convert_to_dict(lines, min_chars=10):
 
 
 def parse_txt(txt_path):
+    """
+    Parse the contents of a TXT file
+    Can contain output from LLM or CSV content
+    :param txt_path: path to TXT file on disk
+    :return: dictionary with guess word keys and taboo word values
+    """
+    # Get the lines from the TXT file
     lines = []
     with open(txt_path) as f:
         for line in f.readlines():
             lines.append(line)
+    # Process the lines
     data_dict = convert_to_dict(lines)
     return data_dict
 
@@ -46,7 +66,10 @@ def parse_txt(txt_path):
 def convert_dict_to_txt(data_dict):
     lines = []
     for guess_word, taboo_words in data_dict.items():
+        # Create the txt line
         taboo_words_str = ",".join(taboo_words)
         line = f"{guess_word},{taboo_words_str}"
+        # Convert apostrophes to underscore because they cause problems in SQL database
+        line = line.split("'", "_")
         lines.append(line)
     return lines
